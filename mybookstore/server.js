0305418e58a6efcb0ec33e1a1a122dd94109c6aa
Chibuikem Ajulu-Okeke <cajuluokeke@gmail.com>
2019-05-06 19:56:33 -0700

    Delete server.js

--- mybookstore/server.js
+++
@@ -1,7 +0,0 @@
-//server.js
-var bookstore = require('./bookstore');
-var port = process.env.PORT || 3001;
-
-var server = bookstore.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
