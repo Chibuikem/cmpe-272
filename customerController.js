fc3ce074b68e101b3d318286598f5803a0100111
Chibuikem Ajulu-Okeke <cajuluokeke@gmail.com>
2019-05-08 10:37:03 -0700

    Update customerController.js

--- customerController.js
+++ customerController.js
@@ -22,11 +22,37 @@ router.post('/', function (req, res) {
         });
 });
 
+//UPDATES A CUSTOMER
+router.put('/:id', function (req, res) {
+    Customer.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, customer) {
+        if (err) return res.status(500).send("There was a problem updating the customer.");
+        res.status(200).send(customer);
+    });
+});
+
+//GETS A CUSTOMER BY ID
+router.get('/:id', function (req, res) {
+    Customer.findById(req.params.id, function (err, customer) {
+        if (err) return res.status(500).send("There was a problem finding the customer.");
+        if (!customer) return res.status(404).send("No Customer found.");
+        res.status(200).send(customer);
+    });
+});
+
+//DELETES A CUSTOMER
+router.delete('/:id', function (req, res) {
+    Customer.findByIdAndRemove(req.params.id, function (err, customer) {
+        if (err) return res.status(500).send("There was a problem deleting the customer.");
+        res.status(200).send("Customer "+ customer.name +" was deleted.");
+    });
+});
+
+
 //RETURNS ALL THE CUSTOMERS IN THE DATABASE
 router.get('/', function (req, res) {
-    Customer.find({}, function (err, customers) {
+    Customer.find({}, function (err, customer) {
         if (err) return res.status(500).send("There was a problem finding the customers.");
-        res.status(200).send(customers);
+        res.status(200).send(customer);
     });
 });
 
